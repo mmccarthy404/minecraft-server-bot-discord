e4a5e9f604d3e3e3c8a732d38b67af28ad3b777b
Michael McCarthy <mmccarthy@rheosrx.com>
2021-02-11 23:22:33 -0500

    Added error checking

--- bot.py
+++ bot.py
@@ -1,6 +1,19 @@
 import discord
 import os
 
+HELP_MESSAGE = """
+I'm a bot used to interact with the Minecraft server!
+-----------------------------------------------------
+
+Example:
+    !minecraft-server <command>
+
+Commands:
+    start - Start Minecraft server
+    stop  - Stop Minecraft server
+    help - Bring up this menu
+""""
+
 client = discord.Client()
 
 @client.event
@@ -13,13 +26,22 @@ async def on_message(message):
         return
 
     if message.content.startswith('!minecraft-server'):
-        command = message.content.split()[1]
+        content_split = message.content.split()
+
+        if len(content_split) == 1:
+            await message.channel.send('ERROR: No input given' + HELP_MESSAGE)
+            return
+
+        command = content_split[1]
 
         if command == 'start':
             await message.channel.send('Starting Minecraft server')
         elif command == 'stop':
             await message.channel.send('Stopping Minecraft server')
+        elif command == 'help':
+            await message.channel.send(HELP_MESSAGE)
         else:
-            await message.channel.send("Unknown command '{command}'".format(command=command))
+            await message.channel.send("ERROR: Unknown command '{command}'".format(command=command) + HELP_MESSAGE)
+            return
 
 client.run(os.getenv('BOT_TOKEN'))
