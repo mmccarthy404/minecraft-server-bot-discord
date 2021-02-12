1aeb712ffc7aecf55bcda413e449c357a928e885
Michael McCarthy <mmccarthy@rheosrx.com>
2021-02-11 22:55:42 -0500

    Created start/stop commands

--- bot.py
+++ bot.py
@@ -12,7 +12,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!minecraft-server'):
+        command = message.content.split()[1]
+
+        if command == 'start':
+            await message.channel.send('Starting Minecraft server')
+        elif command == 'stop':
+            await message.channel.send('Stopping Minecraft server')
+        else:
+            await message.channel.send("Unknown command '{command}'".format(command=command))
 
 client.run(os.getenv('BOT_TOKEN'))
